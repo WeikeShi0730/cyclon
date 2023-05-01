9a9dd265de98c1ab9f4d57ef4dbefbdf88edebbf
Weike Shi <weikeshih@gmail.com>
2023-05-01 19:28:03 -0400

    Fix Map component on mobile

--- src/components/map.component.tsx
+++ src/components/map.component.tsx
@@ -45,8 +45,8 @@ const Map = ({
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position}></Marker>
         <ChangeMapView coords={position} />
