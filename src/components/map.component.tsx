4cc4558fe7f01b5858673a7e38d31205ad9d2d4d
Weike Shi <weikeshih@gmail.com>
2023-04-30 20:57:56 -0400

    Added Map component

--- src/components/map.component.tsx
+++ src/components/map.component.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import dynamic from "next/dynamic";
+
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((module) => module.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((module) => module.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((module) => module.Marker),
+  { ssr: false }
+);
+import "leaflet/dist/leaflet.css";
 
 const Map = ({ running }: { running: boolean }) => {
-  return <div>Map</div>;
+
+  return (
+    <div className="w-96 h-96">
+      <MapContainer
+        style={{ height: "100%", width: "100%" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}></Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
