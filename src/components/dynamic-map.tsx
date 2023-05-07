f76dae9788e6810a9dcc358b52e602896413ff81
Weike Shi <weikeshih@gmail.com>
2023-05-07 15:53:56 -0400

    Fix SVG

--- src/components/dynamic-map.tsx
+++ src/components/dynamic-map.tsx
@@ -1,11 +1,7 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import ChangeMapView from "@/utils/changeMapVeiw";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-const bicycleMarker = new Icon({
-  iconUrl: require("../bicycle-marker.svg"),
-  iconSize: [32, 32],
-});
 
 const DynamicMap = ({ position }: { position: [number, number] }) => {
   return (
@@ -19,11 +15,16 @@ const DynamicMap = ({ position }: { position: [number, number] }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker icon={bicycleMarker} position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker
+        icon={
+          new Icon({
+            iconUrl: "/bicycle-marker.svg",
+            iconSize: [35, 35],
+            iconAnchor: [12, 41],
+          })
+        }
+        position={position}
+      />
       <ChangeMapView coords={position} />
     </MapContainer>
   );
